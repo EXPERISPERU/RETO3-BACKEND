233832bb13718ae8aad3432a911e73674c602565
Mario Robles <140002854+inmobitec@users.noreply.github.com>
2023-08-01 16:43:40 -0500

    inactividad y refresh token

--- backend.services/Controllers/AuthController.cs
+++ backend.services/Controllers/AuthController.cs
@@ -3,9 +3,11 @@ using backend.domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Logging;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace backend.services.Controllers
@@ -17,10 +19,12 @@ namespace backend.services.Controllers
     {
         private readonly string secretKey;
         private readonly IAuthBL service;
+        private readonly int expirationMin;
 
         public AuthController(IConfiguration _configuration, IAuthBL _service) {
             secretKey = _configuration["JWTConfig:secretKey"];
             service = _service;
+            expirationMin = 10;
         }
 
         [HttpPost("[action]")]
@@ -39,7 +43,7 @@ namespace backend.services.Controllers
                 var tokenDescriptor = new SecurityTokenDescriptor
                 {
                     Subject = claims,
-                    Expires = DateTime.UtcNow.AddMinutes(5),
+                    Expires = DateTime.UtcNow.AddMinutes(expirationMin),
                     SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(keyBytes), SecurityAlgorithms.HmacSha256Signature)
                 };
 
@@ -75,6 +79,65 @@ namespace backend.services.Controllers
                 response.errMsj = ex.Message;
                 return StatusCode(500, response);
             }
+        }
+
+        [HttpPost("[action]")]
+        [Authorize]
+        public async Task<ActionResult<ApiResponse<String>>> AuthRefresh()
+        {
+            ApiResponse<String> response = new ApiResponse<String>();
+
+            try
+            {
+                var token = this.Request.Headers.Authorization.First(x => x.StartsWith("Bearer")).Split(' ')[1];
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var tokenSecurity = tokenHandler.ReadToken(token.ToString());
+
+                if (tokenSecurity.ValidTo.AddMinutes(-5) < DateTime.UtcNow) 
+                { 
+
+                    var keyBytes = Encoding.ASCII.GetBytes(secretKey);
+                    var claims = new ClaimsIdentity();
+
+                    claims.AddClaim(UserIdClaim(token));
+
+                    var tokenDescriptor = new SecurityTokenDescriptor
+                    {
+                        Subject = claims,
+                        Expires = DateTime.UtcNow.AddMinutes(expirationMin),
+                        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(keyBytes), SecurityAlgorithms.HmacSha256Signature)
+                    };
+
+                    var tokenConfig = tokenHandler.CreateToken(tokenDescriptor);
+                    token = tokenHandler.WriteToken(tokenConfig);
+                }
+
+                return StatusCode(StatusCodes.Status200OK, new ApiResponse<String> { success = true, data = token, errMsj = "" });
+            }
+            catch (Exception ex)
+            {
+                response.success = false;
+                response.errMsj = ex.Message;
+                return StatusCode(500, response);
+            }
+        }
+
+        private Claim UserIdClaim(string token)
+        {
+            IdentityModelEventSource.ShowPII = true;
+            SecurityToken validatedToken;
+            TokenValidationParameters validationParameters = new TokenValidationParameters();
+
+            validationParameters.ValidateLifetime = true;
+            validationParameters.ValidateAudience = false;
+            validationParameters.ValidateIssuer = false;
+
+            validationParameters.IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey));
+
+            ClaimsPrincipal claimsPrincipal = new JwtSecurityTokenHandler().ValidateToken(token, validationParameters, out validatedToken);
+
+            return claimsPrincipal.Claims.First(c => c.Type == "userId");
         }
     }
 }
