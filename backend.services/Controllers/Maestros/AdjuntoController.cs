4913a7b9fed9e691ad79d1d6d33ec3104437ab40
Mario Robles <45111472+mrobles1693@users.noreply.github.com>
2023-09-22 16:46:24 -0500

    Imp FTP Service

---
+++ backend.services/Controllers/Maestros/AdjuntoController.cs
@@ -0,0 +1,28 @@
+﻿using backend.services.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.services.Controllers.Maestros
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdjuntoController : ControllerBase
+    {
+
+        [HttpPost("[action]")]
+        public async Task<ActionResult<ApiResponse<string>>> UploadFile([FromBody] imbFile file)
+        {
+            FtpClient ftpClient = new FtpClient();
+            var rsp = ftpClient.UploadFile(file);
+
+            return StatusCode(StatusCodes.Status200OK, rsp);
+        }
+
+        [HttpGet("[action]")]
+        public async Task<ActionResult> DownloadFile(string sFile, string sFileExtension)
+        {
+            FtpClient client = new FtpClient();
+            byte[] file = client.DownloadFile(sFile);
+            return File(file, client.sContentType(sFileExtension));
+        }
+    }
+}
