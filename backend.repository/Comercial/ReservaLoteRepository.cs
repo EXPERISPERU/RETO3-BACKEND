d3ef909a01ec1ee501d08ba448771b10a44c3aa0
Mario Robles <45111472+mrobles1693@users.noreply.github.com>
2024-01-26 16:46:28 -0500

    UPD Registro de Reserva

--- backend.repository/Comercial/ReservaLoteRepository.cs
+++ backend.repository/Comercial/ReservaLoteRepository.cs
@@ -43,9 +43,9 @@ namespace backend.repository.Comercial
                 parameters.Add("nIdLote", insReserva.nIdLote);
                 parameters.Add("nIdCliente", insReserva.nIdCliente);
                 parameters.Add("nIdPrecioServicio", insReserva.nIdPrecioServicio);
-                parameters.Add("nIdTipoGestionComercial", insReserva.nIdTipoGestionComercial);
-                parameters.Add("nIdEmpleado", insReserva.nIdEmpleado);
-                parameters.Add("nIdAgenteDealer", insReserva.nIdAgenteDealer);
+                //parameters.Add("nIdTipoGestionComercial", insReserva.nIdTipoGestionComercial);
+                //parameters.Add("nIdEmpleado", insReserva.nIdEmpleado);
+                //parameters.Add("nIdAgenteDealer", insReserva.nIdAgenteDealer);
                 parameters.Add("nIdUsuario_crea", insReserva.nIdUsuario_crea);
 
                 res = await connection.QuerySingleAsync<SqlRspDTO>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
