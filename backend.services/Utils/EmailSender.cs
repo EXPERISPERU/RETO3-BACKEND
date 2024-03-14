fb123217f5811b74ab6635ce7cee7f5180d75236
Mario Robles <45111472+mrobles1693@users.noreply.github.com>
2024-03-14 16:14:31 -0500

    UPD Mejora Variables Email

--- backend.services/Utils/EmailSender.cs
+++ backend.services/Utils/EmailSender.cs
@@ -1,36 +1,29 @@
 ﻿using System.Net.Mail;
 using System.Net;
 using backend.domain;
-using backend.repository.Contacto;
-using backend.businesslogic.Interfaces.Contacto;
 
 namespace backend.services.Utils
 {
     public class EmailSender
     {
-
         /* Variables GMAIL */
         private const string usuariogmail = "sistemas@inmobitec.pe";
-        private const string passwordgmail = "#Inm0bit3c2023.$@1";
         private const string passwordGmailApp = "aikn wnka pasz snxe";
-        private const string hostGmail = "smtp.gmail.com";
-        private const string portGmail = "587";
-        private const string sslGmail = "false";
-        private const string defaultcredentialsGmail = "false";
+        private const string smtpGmail = "smtp.gmail.com";
+        private const int portGmail = 587;
+        private const bool sslGmail = true;
+        private const bool defaultcredentialsGmail = false;
 
         /* Variables OUTLOOK */
         private const string usuarioOutlook = "";
         private const string passwordOutlook = "";
-        private const string hostOutlook = "smtp-mail.outlook.com";
-        private const string portOutlook = "587";
-        private const string sslOutlook = "true";
-        private const string defaultcredentialsOutlook = "true";
-
-
+        private const string smtpOutlook = "smtp-mail.outlook.com";
+        private const int portOutlook = 587;
+        private const bool sslOutlook = true;
+        private const bool defaultcredentialsOutlook = true;
 
         public void SendEmailGmail(String receptor, String asunto, FormularioContactoPortalDTO mensaje, string formato)
         {
-            //getFormatoContactoById
             MailMessage mail = new MailMessage();
 
             mail.From = new MailAddress(usuariogmail);
@@ -40,18 +33,12 @@ namespace backend.services.Utils
             mail.IsBodyHtml = true;
             mail.Priority = MailPriority.Normal;
 
-            String smtpserver = "smtp.gmail.com"; // this._configuration["hostGmail"];
-            int port = int.Parse(portGmail);
-            bool ssl = bool.Parse(sslGmail);
-            bool defaultcreadentials = bool.Parse(defaultcredentialsGmail);
+            SmtpClient smtpClient = new SmtpClient(smtpGmail);
 
-            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
-
-            smtpClient.Host = "smtp.gmail.com"; // smtpserver;
-            smtpClient.Port = 587; // port;
-            smtpClient.EnableSsl = true; // ssl;
-            smtpClient.UseDefaultCredentials = false; //defaultcreadentials;
-
+            smtpClient.Host = smtpGmail;
+            smtpClient.Port = portGmail;
+            smtpClient.EnableSsl = sslGmail;
+            smtpClient.UseDefaultCredentials = defaultcredentialsGmail;
 
             NetworkCredential usercredential = new NetworkCredential(usuariogmail, passwordGmailApp);
 
@@ -59,35 +46,25 @@ namespace backend.services.Utils
             smtpClient.Send(mail);
         }
 
-
         public void SendEmailOutlook(String receptor, String asunto, String mensaje)
         {
             MailMessage mail = new MailMessage();
 
-            String usermail = usuarioOutlook;
-            String passwordmail = passwordOutlook;
-
-            mail.From = new MailAddress(usermail);
-            mail.To.Add(new MailAddress(receptor));
+            mail.From = new MailAddress(usuarioOutlook);
+            mail.To.Add(new MailAddress(passwordOutlook));
             mail.Subject = asunto;
             mail.Body = mensaje;
             mail.IsBodyHtml = true;
             mail.Priority = MailPriority.Normal;
 
-            String smtpserver = hostOutlook;
-            int port = int.Parse(portOutlook);
-            bool ssl = bool.Parse(sslOutlook);
-            bool defaultcreadentials = bool.Parse(defaultcredentialsOutlook);
-
             SmtpClient smtpClient = new SmtpClient();
 
-            smtpClient.Host = smtpserver;
-            smtpClient.Port = port;
-            smtpClient.EnableSsl = ssl;
-            smtpClient.UseDefaultCredentials = defaultcreadentials;
+            smtpClient.Host = smtpOutlook;
+            smtpClient.Port = portOutlook;
+            smtpClient.EnableSsl = sslOutlook;
+            smtpClient.UseDefaultCredentials = defaultcredentialsOutlook;
 
-
-            NetworkCredential usercredential = new NetworkCredential(usermail, passwordmail);
+            NetworkCredential usercredential = new NetworkCredential(usuarioOutlook, passwordOutlook);
 
             smtpClient.Credentials = usercredential;
             smtpClient.Send(mail);
