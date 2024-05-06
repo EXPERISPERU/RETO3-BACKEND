b35072fb66a57d645559c222f7d4ad7f7554ad19
imb-bestrada <159048486+imb-bestrada@users.noreply.github.com>
2024-05-06 16:03:38 -0500

    UPD CONFIGURACIÓN CONCEPTO
    ACTUALIZAR EL CÓDIGO DE CONFIGURACIÓN COMERCIAL X CONCEPTO (TIPO DE COMPROBANTE, MEDIO DE PAGO) 6-5-24

--- backend.businesslogic/Interfaces/Comercial/IConfiguracionConceptoBL.cs
+++ backend.businesslogic/Interfaces/Comercial/IConfiguracionConceptoBL.cs
@@ -14,5 +14,6 @@ namespace backend.businesslogic.Interfaces.Comercial
         Task<IList<ElementoSistemaDTO>> getListMedioPago();
         Task<SqlRspDTO> postInsConfiguracionConcepto(ConfiguracionConceptoDTO configuracion);
         Task<IList<ConfiguracionConceptoDTO>> GetConfiguracionConceptoByIdProyectoAndIdConceptoVenta(int nIdproyecto, int nIdConceptoVenta);
+        Task<IList<JsonFormatDTO>> getComprobanteMedioPago(int nIdCompania);
     }
 }
