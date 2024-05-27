99306c937ed71969b2711e57a75aa3101f6116dc
imb-bestrada <159048486+imb-bestrada@users.noreply.github.com>
2024-05-26 19:59:25 -0500

    UPD FIX CAMBIOS PROCESOS VENTAS
    26-5-2024

--- backend.repository/Interfaces/Comercial/IPreContratoLoteRepository.cs
+++ backend.repository/Interfaces/Comercial/IPreContratoLoteRepository.cs
@@ -9,10 +9,10 @@ namespace backend.repository.Interfaces.Comercial
 {
     public interface IPreContratoLoteRepository
     {
-        Task<IList<SelectDTO>> getSelectPrecioPreContratoByLoteInicial(int nIdLote, decimal nValorInicial);
+        Task<IList<SelectDTO>> getSelectPrecioPreContratoByLoteInicial(int nIdLote, decimal nValorInicial, int nIdMoneda);
         Task<SqlRspDTO> InsPreContratoLote(InsPreContratoLoteDTO insPreContratoLote);
         Task<IList<SelectDTO>> getSelectMedioPago(int nIdUsuario);
         Task<ContratoDTO> getDataPreContratoByLote(int nIdLote, int nIdUsuario);
-        Task<IList<OrdenPagoPreContratoDTO>> getListOPsPreContratoByContrato(int nIdContrato);
+        Task<IList<OrdenPagoPreContratoDTO>> getListOPsPreContratoByContrato(int nIdContrato, int nIdMoneda);
     }
 }
