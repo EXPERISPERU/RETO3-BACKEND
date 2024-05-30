16690f9594fc0c96984bbe02b2ff82a5b8559717
imb-hmedina <166538598+imb-hmedina@users.noreply.github.com>
2024-05-30 11:04:10 -0500

    UPD Maestro Interés cuota
    UPD Maestro Interés cuota

---
+++ backend.services/Controllers/Comercial/InteresCuotaController.cs
@@ -0,0 +1,149 @@
+﻿using backend.businesslogic.Interfaces.Comercial;
+using backend.domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.services.Controllers.Comercial
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class InteresCuotaController : ControllerBase
+    {
+        private readonly IInteresCuotaBL service;
+
+        public InteresCuotaController(IInteresCuotaBL _service)
+        {
+            this.service = _service;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<ActionResult<ApiResponse<InteresCuotaDTO>>> getListInteresCuotaById(int nIdProyecto, int nIdCuotaLote)
+        {
+            ApiResponse<InteresCuotaDTO> response = new ApiResponse<InteresCuotaDTO>();
+
+            try
+            {
+                var result = await service.getListInteresCuotaById(nIdProyecto, nIdCuotaLote);
+
+                response.success = true;
+                response.data = (InteresCuotaDTO)result;
+                return StatusCode(200, response);
+            }
+            catch (Exception ex)
+            {
+                response.success = false;
+                response.errMsj = ex.Message;
+                return StatusCode(500, response);
+            }
+        }
+
+        [HttpPost("[action]")]
+        public async Task<ActionResult<ApiResponse<SqlRspDTO>>> InsInteresCuota([FromBody] InteresCuotaDTO interesCuota)
+        {
+            ApiResponse<SqlRspDTO> response = new ApiResponse<SqlRspDTO>();
+
+            try
+            {
+                var result = await service.InsInteresCuota(interesCuota);
+
+                response.success = result.nCod == 0 ? false : true;
+                response.data = result;
+                return StatusCode(200, response);
+            }
+            catch (Exception ex)
+            {
+                response.success = false;
+                response.errMsj = ex.Message;
+                return StatusCode(500, response);
+            }
+        }
+
+        [HttpGet("[action]")]
+        public async Task<ActionResult<ApiResponse<List<ElementoSistemaDTO>>>> getListMaestroInteres()
+        {
+
+            ApiResponse<List<ElementoSistemaDTO>> response = new ApiResponse<List<ElementoSistemaDTO>>();
+
+            try
+            {
+                var result = await service.getListMaestroInteres();
+
+                response.success = true;
+                response.data = (List<ElementoSistemaDTO>)result;
+                return StatusCode(200, response);
+            }
+            catch (Exception ex)
+            {
+                response.success = false;
+                response.errMsj = ex.Message;
+                return StatusCode(500, response);
+            }
+        }
+
+        [HttpGet("[action]")]
+        public async Task<ActionResult<ApiResponse<ProyectoDTO>>> getProyectoByID(int nIdProyecto)
+        {
+            ApiResponse<ProyectoDTO> response = new ApiResponse<ProyectoDTO>();
+
+            try
+            {
+                var result = await service.getProyectoByID(nIdProyecto);
+
+                response.success = true;
+                response.data = result;
+                return StatusCode(200, response);
+            }
+            catch (Exception ex)
+            {
+                response.success = false;
+                response.errMsj = ex.Message;
+                return StatusCode(500, response);
+            }
+        }
+
+        [HttpGet("[action]")]
+        public async Task<ActionResult<ApiResponse<List<SelectDTO>>>> getSelectTipoDescuento()
+        {
+            ApiResponse<List<SelectDTO>> response = new ApiResponse<List<SelectDTO>>();
+
+            try
+            {
+                var result = await service.getSelectTipoDescuento();
+
+                response.success = true;
+                response.data = (List<SelectDTO>)result;
+                return StatusCode(200, response);
+            }
+            catch (Exception ex)
+            {
+                response.success = false;
+                response.errMsj = ex.Message;
+                return StatusCode(500, response);
+            }
+        }
+
+        [HttpGet("[action]")]
+        public async Task<ActionResult<ApiResponse<List<SelectDTO>>>> getSelectMonedaByCompania(int nIdCompania)
+        {
+            ApiResponse<List<SelectDTO>> response = new ApiResponse<List<SelectDTO>>();
+
+            try
+            {
+                var result = await service.getSelectMonedaByCompania(nIdCompania);
+
+                response.success = true;
+                response.data = (List<SelectDTO>)result;
+                return StatusCode(200, response);
+            }
+            catch (Exception ex)
+            {
+                response.success = false;
+                response.errMsj = ex.Message;
+                return StatusCode(500, response);
+            }
+        }
+
+    }
+}
