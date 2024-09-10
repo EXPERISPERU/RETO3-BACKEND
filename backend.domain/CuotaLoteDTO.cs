b6070aa618700345055b4806754c8080052d9605
Mario Robles <45111472+mrobles1693@users.noreply.github.com>
2024-09-10 13:47:32 -0500

    UPD Cuota Lote

--- backend.domain/CuotaLoteDTO.cs
+++ backend.domain/CuotaLoteDTO.cs
@@ -9,20 +9,12 @@ namespace backend.domain
     public class CuotaLoteDTO
     {
         public int? nIdCuotaLote { get; set; }
-        public int? nIdCompania { get; set; }
-        public string? sCompania { get; set; }
-        public int? nIdProyecto { get; set; }
-        public string? sProyecto { get; set; }
         public int nCuotas { get; set; }
-        public bool bActivo { get; set; }
         public int nIdUsuario_crea { get; set; }
         public string? sUsuario_crea { get; set; }
         public DateTime dFecha_crea { get; set; }
-        public string? sFecha_crea { get; set; }
         public int nIdUsuario_mod { get; set; }
         public string? sUsuario_mod { get; set; }
         public DateTime dFecha_mod { get; set; }
-        public string? sFecha_mod { get; set; }
-        public string? sIdInteres { get; set; }
     }
 }
