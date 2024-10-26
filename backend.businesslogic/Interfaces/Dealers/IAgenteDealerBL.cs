e50bd02a11ce669badba3e938543b5b6e57dac6c
imb-bestrada <159048486+imb-bestrada@users.noreply.github.com>
2024-10-26 13:19:08 -0500

    UPD SEGUIMIENTO - CONTRATO - AGENTE DEALER

--- backend.businesslogic/Interfaces/Dealers/IAgenteDealerBL.cs
+++ backend.businesslogic/Interfaces/Dealers/IAgenteDealerBL.cs
@@ -10,11 +10,12 @@ namespace backend.businesslogic.Interfaces.Dealers
     public interface IAgenteDealerBL
     {
         Task<IList<AgenteDealerDTO>> getListAgenteDealer();
-        Task<AgenteDealerDTO> getAgenteDealerByID(int nIdAgenteDealer);
+        Task<AgenteDealerDTO> getAgenteDealerByID(int nIdAgenteDealer, int nIdCompania);
         Task<AgenteDealerDTO> findAgenteDealer(string? sDNI, string? sCE);
         Task<IList<SelectDTO>> getListGeneros();
         Task<IList<SelectDTO>> getListEstadoCivil();
         Task<SqlRspDTO> InsAgenteDealer(AgenteDealerDTO agenteDealer);
         Task<SqlRspDTO> UpdAgenteDealer(AgenteDealerDTO agenteDealer);
+        Task<IList<SelectDTO>> getListPerfilDealer();
     }
 }
