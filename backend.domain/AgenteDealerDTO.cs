c4060f169268e1e17a7b532720e8bf9604544b75
imb-bestrada <159048486+imb-bestrada@users.noreply.github.com>
2024-10-29 12:16:24 -0500

    UPD AGENTE DEALER

--- backend.domain/AgenteDealerDTO.cs
+++ backend.domain/AgenteDealerDTO.cs
@@ -5,5 +5,6 @@
         public int? nIdAgenteDealer { get; set; }
         public string? sProveedor { get; set; }
         public int? nIdPerfil { get; set; }
+        public int? nIdProveedorAgente { get; set; }
     }
 }
