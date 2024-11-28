b7666cd2c0b37e892b4723799493fc1964f16aed
Jesus Alegre Huaman <jalegreh@inmobi.local>
2024-11-28 16:47:13 -0500

    feat:(ASIENTO) - Formateo de Monedas

--- backend.businesslogic/Utils/Asientos/Caja.cs
+++ backend.businesslogic/Utils/Asientos/Caja.cs
@@ -1,19 +1,22 @@
 ﻿using backend.domain;
+using System.Globalization;
 
 namespace backend.businesslogic.Utils.Asientos
 {
     public class Caja
     {
+        CultureInfo ci_PE = new CultureInfo("es-PE");
+
         public Caja() { }
 
         public string Debe(AsientoCajaDTO asiento, int indice)
         {
-            return $"{asiento.sCajaEfectivo}|{asiento.sAnioMes}|{asiento.sSubdiario}|{indice.ToString().PadLeft(4, '0')}|{asiento.sFechaDocumento}|{asiento.sTipoAnexo}|{asiento.sDNI ?? asiento.sRUC ?? asiento.sCE ?? ""}|{asiento.sAbrevTipoComprobante}|{asiento.sComprobante}|{asiento.sFechaDocumento}|MN|{asiento.nValorTotal}|VTA|{asiento.sFechaDocumento}|{asiento.nTipoCambio}|{asiento.sDescripcionItem}||{asiento.sDescripcionItem}|{asiento.bAnulado}|D|";
+            return $"{asiento.sCajaEfectivo}|{asiento.sAnioMes}|{asiento.sSubdiario}|{indice.ToString().PadLeft(4, '0')}|{asiento.sFechaDocumento}|{asiento.sTipoAnexo}|{asiento.sDNI ?? asiento.sRUC ?? asiento.sCE ?? ""}|{asiento.sAbrevTipoComprobante}|{asiento.sComprobante}|{asiento.sFechaDocumento}|MN|{string.Format(ci_PE, "{0:00.00}", asiento.nValorTotal)}|VTA|{asiento.sFechaDocumento}|{string.Format(ci_PE, "{0:00.00}", asiento.nTipoCambio)}|{asiento.sDescripcionItem}||{asiento.sDescripcionItem}|{asiento.bAnulado}|D|";
         }
 
         public string Haber(AsientoCajaDTO asiento, int indice)
         {
-            return $"{asiento.sCodAsientoProyecto}|{asiento.sAnioMes}|{asiento.sSubdiario}|{indice.ToString().PadLeft(4, '0')}|{asiento.sFechaDocumento}|{asiento.sTipoAnexo}|{asiento.sDNI ?? asiento.sRUC ?? asiento.sCE ?? ""}|{asiento.sAbrevTipoComprobante}|{asiento.sComprobante}|{asiento.sFechaDocumento}|MN|{asiento.nValorTotal}|VTA|{asiento.sFechaDocumento}|{asiento.nTipoCambio}|{asiento.sDescripcionItem}||{asiento.sDescripcionItem}|{asiento.bAnulado}|H|";
+            return $"{asiento.sCodAsientoProyecto}|{asiento.sAnioMes}|{asiento.sSubdiario}|{indice.ToString().PadLeft(4, '0')}|{asiento.sFechaDocumento}|{asiento.sTipoAnexo}|{asiento.sDNI ?? asiento.sRUC ?? asiento.sCE ?? ""}|{asiento.sAbrevTipoComprobante}|{asiento.sComprobante}|{asiento.sFechaDocumento}|MN|{string.Format(ci_PE, "{0:00.00}", asiento.nValorTotal)}|VTA|{asiento.sFechaDocumento}|{string.Format(ci_PE, "{0:00.00}", asiento.nTipoCambio)}|{asiento.sDescripcionItem}||{asiento.sDescripcionItem}|{asiento.bAnulado}|H|";
         }
     }
 }
