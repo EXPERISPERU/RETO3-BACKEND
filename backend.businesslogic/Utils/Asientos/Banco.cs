eaf948915173951721e1d8787c5470ece79e41ae
Jesus Alegre Huaman <jalegreh@inmobi.local>
2024-11-28 17:44:06 -0500

    feat:(ASIENTO) - Correccion Formato de Tipo de Cambio

--- backend.businesslogic/Utils/Asientos/Banco.cs
+++ backend.businesslogic/Utils/Asientos/Banco.cs
@@ -9,12 +9,12 @@ namespace backend.businesslogic.Utils.Asientos
         public Banco() { }
         public string Debe(AsientoBancoDTO asiento, int indice)
         {
-            return $"{asiento.sCuentaContable}|{asiento.sAnioMes}|{asiento.sSubdiario}|{indice.ToString().PadLeft(4, '0')}|{asiento.sFechaDocumento}|02|{(asiento.sDNI ?? asiento.sRUC ?? asiento.sCE ?? "")}|TR|{asiento.sMovimiento}|{asiento.sFechaDocumento}|MN|{string.Format(ci_PE, "{0:00.00}",  asiento.nValorTotal)}|VTA|{asiento.sFechaDocumento}|{string.Format(ci_PE, "{0:00.00}", asiento.nTipoCambio)}|{asiento.sDescripcionItem}||{asiento.sDescripcionItem}|{asiento.bAnulado}|D|003";
+            return $"{asiento.sCuentaContable}|{asiento.sAnioMes}|{asiento.sSubdiario}|{indice.ToString().PadLeft(4, '0')}|{asiento.sFechaDocumento}|02|{(asiento.sDNI ?? asiento.sRUC ?? asiento.sCE ?? "")}|TR|{asiento.sMovimiento}|{asiento.sFechaDocumento}|MN|{string.Format(ci_PE, "{0:00.00}",  asiento.nValorTotal)}|VTA|{asiento.sFechaDocumento}|{string.Format(ci_PE, "{0:0.000}", asiento.nTipoCambio)}|{asiento.sDescripcionItem}||{asiento.sDescripcionItem}|{asiento.bAnulado}|D|003";
         }
 
         public string Haber(AsientoBancoDTO asiento, int indice)
         {
-            return $"{asiento.sCodAsientoProyecto}|{asiento.sAnioMes}|{asiento.sSubdiario}|{indice.ToString().PadLeft(4, '0')}|{asiento.sFechaDocumento}|02|{(asiento.sDNI ?? asiento.sRUC ?? asiento.sCE ?? "")}|{asiento.sAbrevTipoComprobante}|{asiento.sComprobante}|{asiento.sFechaDocumento}|MN|{string.Format(ci_PE, "{0:00.00}", asiento.nValorTotal)}|VTA|{asiento.sFechaDocumento}|{string.Format(ci_PE, "{0:00.00}", asiento.nTipoCambio)}|{asiento.sDescripcionItem}||{asiento.sComprobante}|{asiento.bAnulado}|H|";
+            return $"{asiento.sCodAsientoProyecto}|{asiento.sAnioMes}|{asiento.sSubdiario}|{indice.ToString().PadLeft(4, '0')}|{asiento.sFechaDocumento}|02|{(asiento.sDNI ?? asiento.sRUC ?? asiento.sCE ?? "")}|{asiento.sAbrevTipoComprobante}|{asiento.sComprobante}|{asiento.sFechaDocumento}|MN|{string.Format(ci_PE, "{0:00.000}", asiento.nValorTotal)}|VTA|{asiento.sFechaDocumento}|{string.Format(ci_PE, "{0:0.000}", asiento.nTipoCambio)}|{asiento.sDescripcionItem}||{asiento.sComprobante}|{asiento.bAnulado}|H|";
         }
     }
 }
